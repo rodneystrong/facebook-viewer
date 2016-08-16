809aafb0d2cb4d8a683f1ed866e6562ec827a2bc
aleeex <akang2@gmail.com>
2016-08-16 11:45:39 -0600

    still step 1 including session middleware

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ var express = require('express');
 var expressSession = require('express-session');
 var passport = require('passport');
 var passsport-facebook = require('passsportFacebook');
+//.strategy
+var FacebookStrategy = require('passport-facebook').strategy;
+
+var app = express();
+
+app.use(session({secret: "nerds and thugs"}));
+app.use(passsport.initialize());
+app.use(passsport.session());
+
+app.listen(9090);
