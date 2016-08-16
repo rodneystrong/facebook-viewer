b21731ec78c9be99670748cdea7015f411ba81ba
aleeex <akang2@gmail.com>
2016-08-16 11:48:13 -0600

    defined facebookstrategy for passport.use

--- server.js
+++ server.js
@@ -11,4 +11,12 @@ app.use(session({secret: "nerds and thugs"}));
 app.use(passsport.initialize());
 app.use(passsport.session());
 
+passport.use(new FacebookStrategy({
+  clientID: '',
+  clientSecret: '',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function(token, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
 app.listen(9090);
